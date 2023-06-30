302ac89445bd06f0ab8e6fdc478aa30a8b17712d
+ <+>
2023-06-29 21:07:24 -0400

    created app function to show components

--- sneaker-store/src/App.js
+++ sneaker-store/src/App.js
@@ -3,5 +3,16 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import ProductList from './components/ProductList';
 
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={ProductList} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
