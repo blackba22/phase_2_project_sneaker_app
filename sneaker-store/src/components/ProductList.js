c4afd96ffc70c48a0249edeb0d3a961c11f64272
+ <+>
2023-06-29 20:30:07 -0400

    wrote useEffect function

--- sneaker-store/src/components/ProductList.js
+++ sneaker-store/src/components/ProductList.js
@@ -4,3 +4,13 @@ import axios from 'axios';
 const ProductList = () => {
     const [products, setProducts] = useState([]);
   
+    useEffect(() => {
+        // Fetch products from the server using Axios
+        axios.get('/api/products')
+          .then((response) => {
+            setProducts(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, []);
