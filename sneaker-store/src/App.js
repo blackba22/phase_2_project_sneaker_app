3b467e6dcbd7a2395abd5dd2dba2f06bbc659dd8
+ <+>
2023-06-29 21:01:35 -0400

    added productlist to app.js

--- sneaker-store/src/App.js
+++ sneaker-store/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import ProductList from './components/ProductList';
+
 
 export default App;
