8af103d107408970d187aa0b5e911ab69ee52c5c
+ <+>
2023-07-01 11:21:22 -0400

    got rid of switch error by changing it to router

--- sneaker-store/src/App.js
+++ sneaker-store/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import Home from './components/Home';
 import ProductList from './components/ProductList';
@@ -8,13 +8,12 @@ const App = () => {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={ProductList} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+      </Routes>
     </Router>
   );
 };
 
 export default App;
-
