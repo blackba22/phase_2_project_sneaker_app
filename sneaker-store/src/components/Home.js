96b1d0c9290b2a0d34b4f404618b1057e59172e3
+ <+>
2023-07-01 23:15:54 -0400

    connected json server to frontend

--- sneaker-store/src/components/Home.js
+++ sneaker-store/src/components/Home.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 const Home = () => {
+  const testDeployedAPI = async () => {
+let apiResults = await fetch('https://phase-2-project-json-server-irlz.onrender.com/toys')
+.then(res => res.json())
+.then(data => data)
+.catch(() => (console.log('did not work')))
+console.table(apiResults)
+  };
+
+  useEffect(testDeployedAPI, []);
+
   return <h2>Welcome to Brittany's Sneaker Store!</h2>;
 };
 
